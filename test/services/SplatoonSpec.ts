ad2cc50d2534a53757d3e64ffc79f3d20063727e
Arata Tanaka <tarata43@yahoo.co.jp>
2019-05-23 22:00:16 +0900

    Add test for typescript

---
+++ test/services/SplatoonSpec.ts
@@ -0,0 +1,9 @@
+import * as assert from 'power-assert';
+
+describe('Splatoon', () => {
+    describe('gachi()', async () => {
+        it('hello', async () => {
+            assert(1 === 1);
+        });
+    });
+});
