acfb48cfbd0924ac9c93464b6e36e3578b5e8668
Arata Tanaka <tarata43@yahoo.co.jp>
2019-09-23 02:50:28 +0900

    Refactor discordInit

--- src/Run.ts
+++ src/Run.ts
@@ -1,9 +1,11 @@
 import { init as discordInit } from './applications/Discord';
+import { instance as SplatoonServiceInstance } from './services/Splatoon';
 
 function main(): void {
     const discordToken: string = process.env.DISCORD_TOKEN;
     const userAgent = process.env.USER_AGENT;
-    discordInit(discordToken, userAgent).catch((e) => {
+    const splatoon = SplatoonServiceInstance(userAgent);
+    discordInit(discordToken, splatoon).catch((e) => {
         console.log('Discord Error');
         console.log(e);
     });
